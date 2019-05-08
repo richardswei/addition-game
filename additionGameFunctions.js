5fff78ecef584e1cf16f2e697e32cc6a0641a689
richardswei <50204089+richardswei@users.noreply.github.com>
2019-05-08 15:43:03 -0700

    Update additionGameFunctions.js
    Allow user to use the enter/return button instead of clicking the Check My Answer button

--- additionGameFunctions.js
+++ additionGameFunctions.js
@@ -57,3 +57,18 @@ function newExpression() {
 	document.getElementById("verdict").innerHTML = "";
 	document.getElementById("submission").value = "";
 }
+
+function pressEnter() {
+	// Get the input field
+	var input = document.getElementById("submission");
+	// Execute a function when the user releases a key on the keyboard
+	input.addEventListener("keyup", function(event) {
+	  // Number 13 is the "Enter" key on the keyboard
+	  if (event.keyCode === 13) {
+	    // Cancel the default action, if needed
+	    event.preventDefault();
+	    // Trigger the button element with a click
+	    document.getElementById("submitAnswer").click();
+	  }
+	});
+}
