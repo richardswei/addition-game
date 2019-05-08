e43f27a2f0359dcca94f31f729bfdf937abbd32b
richardswei <wei.richard.s@gmail.com>
2019-05-08 11:45:05 -0700

    add basic skeleton of math app

---
+++ README.md
@@ -0,0 +1,3 @@
+ADDITION GAME
+
+This game is designed to help one of my students get in the habit of understanding how the addition of negative numbers work
---
+++ additionGameFunctions.js
@@ -0,0 +1,53 @@
+/*functions for addition game*/
+
+function generateSignMultiplier() { // returns a 1 or -1 randomly
+	var returnPositive = Math.floor(Math.random() * 2)>0;
+	return returnPositive ? 1 : -1;
+}
+
+function generateExpressionData() {
+	var sign1 = generateSignMultiplier();
+	var sign2 = generateSignMultiplier();
+	/*
+	return a random number from 0 to 9 for num1 and num2 and
+	multiply by their respective signs
+	*/
+	var num1 = Math.floor(Math.random() * 10)*sign1; 
+	var num2 = Math.floor(Math.random() * 10)*sign2;
+	var operand = generateSignMultiplier()>0 ? "+" : "-";
+	var sum = operand==="+" ? num1 + num2 : num1 - num2;
+	/*
+	the string for the expression will have a parentheses around num2 if num2 is neg and operand is "-"
+	*/
+	return {operand:operand, num1:num1, num2:num2, sum:sum};
+}
+
+function parseExpressionObject(expressionData) {
+	var operand = expressionData.operand;
+	var num1 = expressionData.num1;
+	var num2 = expressionData.num2;
+	var sum = expressionData.sum;
+	var displayedExpression =
+		operand==="-" && num2<0 ?
+			num1+" "+operand+" ("+num2+")" :
+				num1+" "+operand+" "+num2;
+	return displayedExpression;
+}
+
+function checkYourAnswer() {
+	var expressionSum = document.getElementById("dataDiv").getAttribute('sum');
+	var submission=document.getElementById("submission").value;
+	var verdict = submission==expressionSum ? "Correct!" : "Wrong :(";
+	document.getElementById("verdict").innerHTML = verdict;
+}
+
+function newExpression() {
+	var expressionData = generateExpressionData();
+	var expressionText = parseExpressionObject(expressionData);
+	var expressionDOM = document.getElementById("expression");
+	expressionDOM.innerHTML=expressionText;
+	var dataDOM = document.getElementById("dataDiv");
+	dataDOM.setAttribute('sum', expressionData.sum);
+	document.getElementById("verdict").innerHTML = "";
+	document.getElementById("submission").value = "";
+}
